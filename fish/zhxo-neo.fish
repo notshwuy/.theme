2b81f3aa1581c70b684c7f210ed7a40936bc7e22
赵雨愛 <47329939+sxhk0@users.noreply.github.com>
2024-02-02 05:58:33 -0300

    feat: zhxo's fish theme

---
+++ fish/zhxo-neo.fish
@@ -0,0 +1,39 @@
+#!/usr/bin/env fish
+
+set -U fish_color_autosuggestion 949494
+set -U fish_color_cancel --reverse
+set -U fish_color_command afafff
+set -U fish_color_comment 808080
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_end ff8787
+set -U fish_color_error ff5f5f
+set -U fish_color_escape brcyan
+set -U fish_color_history_current --bold
+set -U fish_color_host normal
+set -U fish_color_host_remote
+set -U fish_color_keyword
+set -U fish_color_normal normal
+set -U fish_color_operator brcyan
+set -U fish_color_option
+set -U fish_color_param ffffff
+set -U fish_color_quote 5fffaf
+set -U fish_color_redirection ffaf5f --bold
+set -U fish_color_search_match bryellow --background=brblack
+set -U fish_color_selection white --bold --background=brblack
+set -U fish_color_status red
+set -U fish_color_user brgreen
+set -U fish_color_valid_path --underline
+set -U fish_pager_color_background
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description yellow --italics
+set -U fish_pager_color_prefix normal --bold --underline
+set -U fish_pager_color_progress brwhite --background=cyan
+set -U fish_pager_color_secondary_background
+set -U fish_pager_color_secondary_completion
+set -U fish_pager_color_secondary_description
+set -U fish_pager_color_secondary_prefix
+set -U fish_pager_color_selected_background --reverse
+set -U fish_pager_color_selected_completion
+set -U fish_pager_color_selected_description
+set -U fish_pager_color_selected_prefix
